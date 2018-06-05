d55f7ecf0977df4abf6a9dc11c2006f7732da801
Northtwig <nordqvist.gaming@gmail.com>
2018-06-06 10:07:48 +1200

    Resolved movement. Switched Colors. Updated Movement. Organized Code.

--- Assets/Airship.cs
+++ Assets/Airship.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Airship : MonoBehaviour {
 
-	Rigidbody rigidBody;
+    [SerializeField]
+    float lateralThrust, mainThrust;
+
+    Rigidbody rigidBody;
 	AudioSource audioSource;
 
 	// Use this for initialization
@@ -19,26 +20,50 @@ public class Airship : MonoBehaviour {
 		Rotate ();
 	}
 
-	private void Rotate () {
-		if (Input.GetKey(KeyCode.A)) {
-			transform.Rotate (Vector3.forward);
-		} else if (Input.GetKey(KeyCode.D)) {
-			transform.Rotate (-Vector3.forward);
-		}
-	}
+    void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print("Safe");
+                break;
+            case "Fuel":
+                print("Topped Up");
+                break;
+            default:
+                print("Dead");
+                //kill player
+                break;
+        }
 
-	private void Thrust ()
-	{
-		if (Input.GetKey (KeyCode.Space)) {
-			//Can thrust while rotating
-			rigidBody.AddRelativeForce (Vector3.up);
-			if (!audioSource.isPlaying) {
-				//So it doesn't layer
-				audioSource.Play ();
-			}
+    }
+
+    private void Thrust() {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Can thrust while rotating
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                //So it doesn't layer sound
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate () {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rotationThisFrame = lateralThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate (Vector3.forward * rotationThisFrame);
+		} else if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate (-Vector3.forward * rotationThisFrame);
 		}
-		else {
-			audioSource.Stop ();
-		}
-	}
+
+        rigidBody.freezeRotation = false;
+    }
 }
